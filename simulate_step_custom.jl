ad70e8d2118d01c24470549db8763b1de8e696f5
Xentrics <bastiseelbinder@web.de>
2024-09-12 13:50:23 +0200

    Add border wrap support

--- simulate_step_custom.jl
+++ simulate_step_custom.jl
@@ -1,7 +1,32 @@
-using Plots, DSP
+using Plots, DSP, ArrayPadding
+
+# a single game of life step with custom rules & border folding
+function step_fold(GoL, al_min=2, al_max=3, new_min=3, new_max=3)
+
+    # 2D kernel to update cells        
+    Mfilt = [true true  true;
+             true false true;
+             true true  true]
+    
+    # Add a border mirroring 'the other side' of the matrix
+    GoL2 = pad(GoL, :periodic, 1)
+    
+    # Perform 2D FFT-based convolution
+    convGoL = DSP.conv(GoL2, Mfilt)
+    stay_alive = (convGoL .>= al_min  .&& convGoL .<= al_max )
+    born_new   = (convGoL .>= new_min .&& convGoL .<= new_max)
+
+    # Keep only cells with 2 (or 3) neighbours alive
+    # Make new cells if neighbours are 3
+    twoLiveNeig = (GoL .&& stay_alive[3:end-2, 3:end-2])
+    GoL = twoLiveNeig .|| born_new[3:end-2, 3:end-2]
+    
+    return(GoL)
+end
+
 
 # a single game of life step with custom rules
-function step_custom(GoL, al_min=2, al_max=3, new_min=3, new_max=3, fold=true)
+function step_custom(GoL, al_min=2, al_max=3, new_min=3, new_max=3)
 
     # 2D kernel to update cells        
     Mfilt = [true true  true;
@@ -23,13 +48,17 @@ end
 
 
 # main Game of life function loop
-function runGoL_custom(GoL; runs=100, al_min=2, al_max=3, new_min=3, new_max=3, sleepTime=0.02, safeToDisk=false, showOnConsole=true)
+function runGoL_custom(GoL; runs=100, al_min=2, al_max=3, new_min=3, new_max=3, sleepTime=0.02, fold=true, safeToDisk=false, showOnConsole=true)
 
     # Simulate the game
     for i_gen = 1:runs
-
-        GoL = step_custom(GoL, al_min, al_max, new_min, new_max)
-          
+        
+        if fold
+            GoL = step_fold(GoL, al_min, al_max, new_min, new_max)
+        else
+            GoL = step_custom(GoL, al_min, al_max, new_min, new_max)
+        end
+        
         if showOnConsole
             plot(heatmap(GoL), show=true, reuse=true)
             sleep(sleepTime)
